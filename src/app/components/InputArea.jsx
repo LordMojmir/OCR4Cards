ac74617377ee4fd6abb8d83fe7d95d02e77727c4
LordMojmir <75137860+LordMojmir@users.noreply.github.com>
2024-05-28 11:17:33 +0200

    making the adding of error message

--- src/app/components/InputArea.jsx
+++ src/app/components/InputArea.jsx
@@ -49,14 +49,15 @@ export default function Home() {
 
                 const userId = "oq6e7XMHmZ5liQafLjf8"; // Specific user ID
                 await saveOcrDataToFirebase(userId, response.data.data);
+                setPreview(null)
+                setBackendState("finished")
             } else {
+                setBackendState("error")
                 console.error("Error with status code:", response.status);
             }
         } catch (error) {
+            setBackendState("error")
             console.error("Error uploading image:", error);
-        } finally {
-            setBackendState("finished")
-
         }
 
     };
@@ -77,16 +78,34 @@ export default function Home() {
                 )}
             </div>
 
+            {backendRunning === "error" && (
+                <div onClick={() => setBackendState("finished")}
+                    className="flex items-center justify-center p-4 bg-red-300 border border-red-400 text-red-700 rounded relative m-3"
+                    role="alert">
+                    <strong className="font-bold mr-2">Error:</strong>
+                    <span className="block sm:inline">Something went wrong.</span>
+                    <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20">
+                    <title >Close</title>
+                    <path
+                    d="M14.348 5.652a1 1 0 00-1.414 0L10 8.586 7.066 5.652a1 1 0 10-1.414 1.414L8.586 10l-2.934 2.934a1 1 0 101.414 1.414L10 11.414l2.934 2.934a1 1 0 001.414-1.414L11.414 10l2.934-2.934a1 1 0 000-1.414z"/>
+                    </svg>
+                    </span>
+                </div>
+            )}
+
+
             <button
                 onClick={handleSubmit}
                 className=" disabled:opacity-50 bg-blue-950 text-white px-5 py-2 w-1/2 rounded"
                 disabled={backendRunning === "submitted"}
             >
                 <div className="flex h-full items-center gap-4 justify-between">
-                Submit
-                {backendRunning === "submitted" && (
-                    <SpinnerElement/>
-                )}
+                    Submit
+                    {backendRunning === "submitted" && (
+                        <SpinnerElement/>
+                    )}
                 </div>
             </button>
             {result && (
